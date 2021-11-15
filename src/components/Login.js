eaa55248d9f1800771ad7b13d7478e6fa7ed363a
VictorKildahl <victor.petersen2@gmail.com>
2021-11-15 13:48:59 +0100

    test

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 function Login() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
-  const [jwt, setJwt] = useState();
+  const [jwt, setJwt] = useState(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiTWFuYWdlciIsIlJvbGUiOiJNYW5hZ2VyIiwiVXNlcklkIjoiMSIsIm5iZiI6IjE2MzY5Nzk5NTEiLCJleHAiOiIxNjM3MDY2MzUxIn0.02I0PIyISvH85lBFy15hfL2q4CPfPBkJEKKnkbNXvZU"
+  );
 
   //   const login = async () => {
   //     console.log("username" + username);
@@ -23,24 +25,25 @@ function Login() {
   //     setJwt(result.data);
   //   };
 
-  const login = async () => {
+  const login = async (username, password) => {
     console.log("username" + username);
     console.log("password" + password);
 
-    const result = fetch(
-      "https://afe2021fitness.azurewebsites.net/api/Users/login",
-      {
-        method: "POST",
-        headers: {},
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      }
-    );
-
-    console.log(result.data);
-    setJwt(result.data);
+    fetch("https://afe2021fitness.azurewebsites.net/api/Users/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        mode: "cors",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }).then((response) => {
+      response.json();
+      console.log("response" + response.jwt);
+    });
   };
 
   return (
@@ -49,17 +52,28 @@ function Login() {
         Username
         <input
           type="text"
-          onChange={(event) => setUsername(event.target.value)}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
         />
       </label>
       <label>
         Password:
         <input
           type="text"
-          onChange={(event) => setPassword(event.target.value)}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
       </label>
-      <input type="submit" value="Login" onClick={() => login()} />
+      <input
+        type="submit"
+        value="Login"
+        onClick={(event) => {
+          event.preventDefault();
+          login(username, password);
+        }}
+      />
     </form>
   );
 }
